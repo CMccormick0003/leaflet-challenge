55a388eaeae432ce4bab731884df263e9f0721b0
CMccormick0003 <calogera.mccormick@gmail.com>
2023-05-29 13:02:01 -0400

    final commit

--- Starter_Code/Leaflet-Part-1/js/logic.js
+++ Starter_Code/Leaflet-Part-1/js/logic.js
@@ -9,8 +9,8 @@ let basemap = L.tileLayer(
 );
 
 let map = L.map("map", {
-    center: [30, -55],
-    zoom: 2.75
+    center: [40, -10],
+    zoom: 2.4
 })
 
 basemap.addTo(map);
@@ -25,50 +25,50 @@ function getRadius(magnitude) {
 
 // Show depth of the earthquake by color
 function getColor(depth) {
-    if(depth > -1) {
-        return "#000000"
+    if(depth === 0) {
+        return "#ffffe6"
     }
-    else if (depth > 5) {
-        return "#fffdc8"
+    else if (depth < 5) {
+        return "#ffffaa"
     }
-    else if (depth > 10) {
-        return "#ffcb64"
+    else if (depth < 10) {
+        return "#ffee99"
     }
-    else if (depth > 20) {
-        return "#9997fc"
+    else if (depth < 20) {
+        return "#ffcc88"
     }
-    else if (depth > 30) {
-        return "#6665fd"
+    else if (depth < 30) {
+        return "#ffbb77"
     }
-    else if (depth > 40) {
-        return "#9965fd"
+    else if (depth < 40) {
+        return "#ffaa66"
     }
-    else if (depth > 50) {
-        return "#bb32fe"
+    else if (depth < 50) {
+        return "#ff9955"
     }
-    else if (depth > 60) {
-        return "#ff32fe"
+    else if (depth < 60) {
+        return "#ff8855"
     }
-    else if (depth > 70) {
-        return "#ff00ff"
+    else if (depth < 70) {
+        return "#ff7755"
     }
-    else if (depth > 80) {
-        return "#ff00aa"
+    else if (depth < 80) {
+        return "#ff7733"
     }
-    else if (depth > 90) {
-        return "#ff0055"
+    else if (depth < 90) {
+        return "#ff6622"
     }
-    else if (depth > 100) {
-        return "#cc0044"
+    else if (depth < 100) {
+        return "#ff3322"
     }
-    else if (depth > 150) {
-        return "#990033"
+    else if (depth < 150) {
+        return "#ff1111"
     }
-    else if (depth > 300) {
-        return "#000000"
+    else if (depth < 300) {
+        return "#660000"
     }
     else {
-        return "#ffffff"
+        return "#000000"
    }
 }
 
@@ -103,25 +103,30 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojs
         }).addTo(map);
     
 
- // Add map legend
- let legend = L.control({
-    position:"bottomleft"
+// Add map legend
+let legend = L.control({
+    position: "bottomleft"
 });
 
-legend.onAdd = function(){
-    let container = L.DomUtil.create("div", "info legend");
+legend.onAdd = function () {
+    let div = L.DomUtil.create("div", "info legend");
+
+    // Add legend title
+    div.innerHTML = "<strong>Earthquake depth (km)</strong><br>";
+
     let grades = [0, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 300];
-    let colors = ["#000000", "#fffdc8", "#ffcb64", "#9997fc", "#6665fd", "#9965fd", "#bb32fe", "#ff32fe", "#ff00ff", "#ff00aa", "#ff0055", "#cc0044", "#990033", "#000000"];
-    // for(let index = 0; index < grades.length; index++) {
-    //    container.innerHTML += `<i style="background: ${colors[index]}"></i>${grades[index]}+ <br>`
-    //}
+    let colors = ["#ffffe6", "#ffffaa", "#ffee99", "#ffcc88", "#ffbb77", "#ffaa66", "#ff9955", "#ff8855", "#ff7755", "#ff7733", "#ff6622", "#ff3322", "#ff1111", "#660000"];
 
     for (let i = 0; i < grades.length; i++) {
-        div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
-          + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+        div.innerHTML +=
+            "<span class='legend-item' style='background-color: " + colors[i] + "'></span>" +
+            grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
     }
 
     return div;
-}
-    legend.addTo(map);
-})    
+
+};
+
+legend.addTo(map);
+        
+});    
