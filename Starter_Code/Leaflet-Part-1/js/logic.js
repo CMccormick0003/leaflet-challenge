d8bc9ca28a54cd4f7eb4c57a9d86996f6036227e
CMccormick0003 <calogera.mccormick@gmail.com>
2023-05-28 23:24:47 -0400

    add D3 for json from earthquake website

--- Starter_Code/Leaflet-Part-1/js/logic.js
+++ Starter_Code/Leaflet-Part-1/js/logic.js
@@ -1,3 +1,6 @@
+// Add console.log
+console.log("working");
+
 // Make the basemap with OpenStreetMap
 let basemap = L.tileLayer(
     "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
@@ -12,6 +15,14 @@ let map = L.map("map", {
 })
 
 basemap.addTo(map);
+
+// Create a marker based on the magnitude - larger radius for higher magnitude
+function getRadius(magnitude) {
+    if(magnitude === 0) {
+        return 1
+    }
+    return magnitude * 4
+}
 
 // Show depth of the earthquake by color
 function getColor(depth) {
@@ -73,3 +84,35 @@ function getColor(depth) {
         return "#f3eff5"
     }
 }
+
+// Get the earthquake data in json format from last 30 days from earthquake.usgs.gov
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson").then(function(data){
+    function styleInfo(feature){
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: getColor(feature.geometry.coordinates[2]),
+            color: "#FFFFFF",
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.4
+        }
+    }
+
+// Pull the json data.  Set the marker shape as a circle.
+        L.geoJson(data, {
+            pointToLayer: function(feature,latlng){
+                return L.circleMarker(latlng);
+            },
+            style: styleInfo,
+            onEachFeature: function(feature,layer){
+                layer.bindPopup(`
+                    Magnitude: ${feature.properties.mag} <br>
+                    Depth: ${feature.geometry.coordinates[2]} <br>
+                    Location: ${feature.properties.place}
+                `);
+            }
+        }).addTo(map);
+    
+}    
